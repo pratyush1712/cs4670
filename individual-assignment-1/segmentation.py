d6bd49186755b8cf76e67a07ce4c03c5ad45614e
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2023-03-26 05:32:00 -0400

    idk

--- individual-assignment-1/segmentation.py
+++ individual-assignment-1/segmentation.py
@@ -12,11 +12,11 @@ def construct_graph(img):
             ii >= 0 and ii < height and jj >= 0 and jj < width and (ii != i or jj != j)
         )
 
+    def w(i,j,ii,jj):
+        return np.exp(-100 * np.linalg.norm(img[i, j] - img[ii, jj]) ** 2)
+
     length = height * width
     graph = np.zeros((length, length))
-    get_w = lambda i, j, ii, jj: np.exp(
-        -100 * np.linalg.norm(img[i, j] - img[ii, jj]) ** 2
-    )
     for i in range(height):
         for j in range(width):
             first_loc = loc(i, j)
@@ -27,17 +27,14 @@ def construct_graph(img):
                     jj = j + jj_offset
                     if valid_point(i, j, ii, jj):
                         second_loc = loc(ii, jj)
-                        graph[first_loc, second_loc] = get_w(i, j, ii, jj)
+                        graph[first_loc, second_loc] = w(i, j, ii, jj)
     return graph
 
 
 def graph_based_segmentation(img):
     graph = construct_graph(img)
     D = np.diag(np.sum(graph, axis=1))
-    I = np.identity(graph.shape[0])
-    D_inv = np.linalg.inv(D)
-    A = I - (D_inv @ graph)
-    vals, vecs = np.linalg.eig(A)
-    second_smallest_index = np.argsort(vals)[1]
-    second_smallest_eig_vec = vecs[:, second_smallest_index]
-    return np.reshape(second_smallest_eig_vec, (img.shape[0], img.shape[1]))
+    A = np.identity(graph.shape[0]) - np.linalg.inv(D) @ graph
+    eig_vals, eig_vecs = np.linalg.eigh(A)
+    second_smallest_eig_vec = eig_vecs[:, np.argsort(eig_vals)[1]]
+    return np.reshape(second_smallest_eig_vec, img.shape[:2])
